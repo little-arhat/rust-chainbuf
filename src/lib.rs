665588fa2eb30f7af54f9af7578ee3f55c7399b8
Roma Sokolov <sokolov.r.v@gmail.com>
2014-09-18 23:27:32 +0400

    [change] cargo-fication complete

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#![crate_name = "chainbuf"]
+#![comment = "Fast chained buffers"]
+#![license = "MIT"]
+
+// #![deny(missing_doc)]
+#![deny(warnings)]
+
+
+//! The main crate for the Chainbuf library.
+//!
+//! ... docs are to be written
+//!
+
+// Stdlib dependencies
+#[cfg(test)] extern crate test;
+
+extern crate collections;
+
+
+
+pub use chainbuf::Chain;
+
+// internal
+mod chainbuf;
