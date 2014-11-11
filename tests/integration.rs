ed38f0d80fea6924a3ee98db3125c11086df7534
Roma Sokolov <sokolov.r.v@gmail.com>
2014-11-11 12:54:12 +0300

    [compat] rtio was removed, use nix-rust's routines for tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,5 +1,4 @@
 extern crate chainbuf;
-extern crate native;
 #[cfg(feature="nix")] extern crate nix;
 
 #[cfg(test)]
@@ -7,8 +6,7 @@ mod test {
     #[cfg(feature="nix")]
     mod test_writev {
         use chainbuf::Chain;
-        use nix::unistd::{pipe, close};
-        use native::io::FileDesc;
+        use nix::unistd::{pipe, close, read};
         use std::rand::{task_rng, Rng};
 
         #[test]
@@ -29,7 +27,6 @@ mod test {
             let pipe_res = pipe();
             assert!(pipe_res.is_ok());
             let (reader, writer) = pipe_res.ok().unwrap();
-            let mut reader = FileDesc::new(reader, true);
             // write all data
             let write_res = chain.write_to_fd(writer, None, None);
             assert!(write_res.is_ok());
@@ -41,13 +38,14 @@ mod test {
             // chain has been drained
             assert_eq!(chain.len(), 0);
             let mut read_buf = Vec::from_elem(128 * 16, 0u8);
-            let read_res = reader.inner_read(read_buf.as_mut_slice());
+            let read_res = read(reader, read_buf.as_mut_slice());
             assert!(read_res.is_ok());
             let read = read_res.ok().unwrap() as uint;
             // Check we have read as much as we written
             assert_eq!(read, written);
             assert_eq!(to_write.as_slice(), read_buf.as_slice());
             let _ = close(writer);
+            let _ = close(reader);
         }
 
     }
